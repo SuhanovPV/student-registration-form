d2659f858fe1af00c75e2221b5877828a40eb121
Sukhanov Pavel <ssuxxarr@gmail.com>
2024-11-01 22:08:44 +0300

    init commit (commit from https://github.com/SuhanovPV/qa_guru_hw5)

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_set():
+
+    options = webdriver.FirefoxOptions()
+    options.page_load_strategy = 'eager'
+
+    browser.config.driver_options = options
+    browser.config.window_height = '1080'
+    browser.config.window_width = '1920'
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+
+    yield
+
+    browser.close()
