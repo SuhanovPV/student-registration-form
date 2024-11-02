259568e7af0084df390766fddfb8ca650bfcfd11
Sukhanov Pavel <ssuxxarr@gmail.com>
2024-11-02 14:03:23 +0300

    Create Fluetn page object, change verification data to tupe

--- student_registration_form/pages/registration_page.py
+++ student_registration_form/pages/registration_page.py
@@ -7,61 +7,68 @@ class RegistrationPage:
 
     def open(self):
         browser.open('/automation-practice-form')
+        return self
 
     def fill_first_name(self, value):
         browser.element('#firstName').should(be.blank).type(value)
+        return self
 
     def fill_last_name(self, value):
         browser.element('#lastName').should(be.blank).type(value)
+        return self
 
     def fill_email(self, value):
         browser.element('#userEmail').should(be.blank).type(value)
+        return self
 
     def select_gender(self, value):
         browser.all('[name=gender]').element_by(have.value(value)).element('..').click()
+        return self
 
     def fill_mobile(self, value):
         browser.element('#userNumber').should(be.blank).type(value)
+        return self
 
     def fill_date_of_birth(self, year, month, day):
         browser.element('#dateOfBirthInput').click()
         browser.element(f'.react-datepicker__year-select > option[value="{year}"]').click()
         browser.element(f'.react-datepicker__month-select > option[value="{month}"]').click()
         browser.element(f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)').click()
+        return self
 
     def fill_subjects(self, value):
         browser.element('#subjectsInput').type(value)
         browser.element('#subjectsContainer div[id^="react-select"]').click()
+        return self
 
     def select_hobbies(self, value):
         browser.all('[id^=hobbies-checkbox-]+label').element_by(have.exact_text(value)).click()
+        return self
 
     def fill_current_address(self, value):
         browser.element('#currentAddress').type(value)
+        return self
 
     def upload_picture(self, value):
         browser.element("#uploadPicture").send_keys(resources.path(value))
+        return self
 
     def select_state(self, value):
         browser.element('#state').click()
         browser.all('#state div[id^="react-select-"]').element_by(have.text(value)).click()
+        return self
 
     def select_city(self, value):
         browser.element('#city').click()
         browser.all('#city div[id^="react-select-"]').element_by(have.text(value)).click()
+        return self
 
     def submit(self):
         browser.element('#submit').click()
+        return self
 
-    def should_have_register_user_with(self, full_name, email, gender, mobile, date_of_birth, subjects, hobbies,
-                                       picture, current_address, state_and_city):
-        browser.element('//td[preceding-sibling::td="Student Name"]').should(have.exact_text(full_name))
-        browser.element('//td[preceding-sibling::td="Student Email"]').should(have.exact_text(email))
-        browser.element('//td[preceding-sibling::td="Gender"]').should(have.exact_text(gender))
-        browser.element('//td[preceding-sibling::td="Mobile"]').should(have.exact_text(mobile))
-        browser.element('//td[preceding-sibling::td="Date of Birth"]').should(have.exact_text(date_of_birth))
-        browser.element('//td[preceding-sibling::td="Subjects"]').should(have.exact_text(subjects))
-        browser.element('//td[preceding-sibling::td="Hobbies"]').should(have.exact_text(hobbies))
-        browser.element('//td[preceding-sibling::td="Picture"]').should(have.exact_text(picture))
-        browser.element('//td[preceding-sibling::td="Address"]').should(have.exact_text(current_address))
-        browser.element('//td[preceding-sibling::td="State and City"]').should(have.exact_text(state_and_city))
+    def should_have_register_user_with(self, *verification_data):
+
+        for field, value in verification_data:
+            browser.element(f'//td[preceding-sibling::td="{field}"]').should(have.exact_text(value))
+
--- tests/test_form.py
+++ tests/test_form.py
@@ -6,32 +6,33 @@ def test_fill_form():
     registration_page.open()
 
     # WHEN
-    registration_page.fill_first_name('Pavel')
-    registration_page.fill_last_name('Sukhar')
-    registration_page.fill_email('sukhar@mail.com')
-    registration_page.select_gender('Male')
-    registration_page.fill_mobile('8800535653')
-    registration_page.fill_date_of_birth('1988', '3', '05')
-    registration_page.fill_subjects('Maths')
-    registration_page.fill_subjects('Computer Science')
-    registration_page.select_hobbies('Sports')
-    registration_page.select_hobbies('Music')
-    registration_page.fill_current_address('Bawana Rd, Shahabad Daulatpur, Shahabad Daulatpur Village, Rohini')
-    registration_page.upload_picture('pic.png')
-    registration_page.select_state('Rajasthan')
-    registration_page.select_city('Jaipur')
-    registration_page.submit()
+    registration_page \
+        .fill_first_name('Pavel') \
+        .fill_last_name('Sukhar') \
+        .fill_email('sukhar@mail.com') \
+        .select_gender('Male') \
+        .fill_mobile('8800535653') \
+        .fill_date_of_birth('1988', '3', '05') \
+        .fill_subjects('Maths') \
+        .fill_subjects('Computer Science') \
+        .select_hobbies('Sports') \
+        .select_hobbies('Music') \
+        .fill_current_address('Bawana Rd, Shahabad Daulatpur, Shahabad Daulatpur Village, Rohini') \
+        .upload_picture('pic.png') \
+        .select_state('Rajasthan') \
+        .select_city('Jaipur') \
+        .submit()
 
     # THEN
     registration_page.should_have_register_user_with(
-        "Pavel Sukhar",
-        'sukhar@mail.com',
-        'Male',
-        '8800535653',
-        '05 April,1988',
-        'Maths, Computer Science',
-        'Sports, Music',
-        'pic.png',
-        'Bawana Rd, Shahabad Daulatpur, Shahabad Daulatpur Village, Rohini',
-        'Rajasthan Jaipur'
+        ('Student Name','Pavel Sukhar'),
+        ('Student Email','sukhar@mail.com'),
+        ('Gender','Male'),
+        ('Mobile', '8800535653'),
+        ('Date of Birth', '05 April,1988'),
+        ('Subjects','Maths, Computer Science'),
+        ('Hobbies', 'Sports, Music'),
+        ('Picture', 'pic.png'),
+        ('Address', 'Bawana Rd, Shahabad Daulatpur, Shahabad Daulatpur Village, Rohini'),
+        ('State and City','Rajasthan Jaipur')
     )
